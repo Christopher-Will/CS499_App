363c8276f51066eeb2ab65cff2b4b78948e63bb1
Chris Will <c.will186@gmail.com>
2017-10-15 20:03:39 -0400

    initial test

--- app/src/main/java/com/example/christopher/cs499/MainActivity.java
+++ app/src/main/java/com/example/christopher/cs499/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.christopher.cs499;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
-        Button createNewAccountButton = (Button) findViewById(R.id.button);
+        Button createNewAccountButton = (Button) findViewById(R.id.createAccountButton);
         createNewAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, createAccount.class));
+                //setContentView(R.layout.create_account);
+            }
+        });
 
+
+        final EditText email = (EditText) findViewById(R.id.emailFieldAccount);
+        final EditText password = (EditText) findViewById(R.id.passwordFieldAccount);
+        Button signInButton = (Button) findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(email.length() == 0 || password.length() == 0){
+                    TextView errorMessage = (TextView) findViewById(R.id.signInError);
+                    errorMessage.setText("Email and Password fields cannot be empty");
+                    errorMessage.setTextColor(Color.RED);
+                }else{
+                    //query their email and password
+                }
             }
         });
 
     }
+
+
 }
--- app/src/main/java/com/example/christopher/cs499/createAccount.java
+++ app/src/main/java/com/example/christopher/cs499/createAccount.java
@@ -1,13 +1,65 @@
 package com.example.christopher.cs499;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class createAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.create_account);
+        Button btn = (Button) findViewById(R.id.makeAccountButton);
+        Log.d("TEST!!!!!", "world");
+
+
+        Button createAccount = (Button) findViewById(R.id.makeAccountButton);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstName = (EditText) findViewById(R.id.firstNameField);
+                EditText lastName = (EditText) findViewById(R.id.lastNameField);
+                EditText address = (EditText) findViewById(R.id.addressField);
+                EditText email = (EditText) findViewById(R.id.emailFieldAccount);
+                EditText password = (EditText) findViewById(R.id.passwordFieldAccount);
+                EditText confirmPassword = (EditText) findViewById(R.id.confirmPasswordField);
+
+                /*
+                TextView fNameError = (TextView) findViewById(R.id.firstNameError);
+                TextView lNameError = (TextView) findViewById(R.id.lastNameError);
+                TextView addressError = (TextView) findViewById(R.id.addressError);
+                TextView emailError = (TextView) findViewById(R.id.emailError);
+                TextView passwordError = (TextView) findViewById(R.id.passwordError);
+
+
+                if(firstName.length() == 0){
+                    fNameError.setText("Please enter your first name");
+                    fNameError.setTextColor(Color.RED);
+                    Log.d("length is 0", "length is 0");
+                }
+                if(lastName.length() == 0){
+                    lNameError.setText("Please enter your last name");
+                    lNameError.setTextColor(Color.RED);
+                }
+                if(email.length() == 0){
+                    emailError.setText("Please enter your email");
+                    emailError.setTextColor(Color.RED);
+                }
+                if(password != confirmPassword){
+                    passwordError.setText("Passwords do not match");
+                    passwordError.setTextColor(Color.RED);
+                }
+                */
+
+
+            }
+        });
+
     }
 }
